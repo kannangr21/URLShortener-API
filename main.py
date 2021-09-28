3cd17176ed1ad7c8e74fa5b74acb43749621823b
kannangr21 <kannanravi2105@gmail.com>
2021-09-29 00:22:56 +0530

    OAuth2 implementation
    Only update endpoint works with OAuth2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
 
+import jwt
 import uvicorn
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from sqlalchemy.orm.session import Session
 
@@ -12,6 +14,8 @@ import schemas
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "users/login")
+JWT_SECRET = "AsecretKeyTobeSecret"
 app = FastAPI()
 origins = [
     "http://localhost.tiangolo.com",
@@ -61,20 +65,26 @@ def create_user(user : schemas.User, db : Session = Depends(get_db)):
 
 @app.post("/users/login")
 def user_login(uname : str, password : str,db : Session = Depends(get_db)):
+    print("It comes here")
     db_user = db.query(models.User).filter(models.User.userName == uname).first()
     if db_user:
         if pwd_context.verify(password, db_user.password):
-            return db_user
+            token = jwt.encode({"userName" : db_user.userName, "email" : db_user.email}, JWT_SECRET)
+           
+            return db_user,{"access_token" : token, "token_type" : "bearer"}
         else:
             return {"Error":"Password Doesn't Match"}
     db.close()
     return {"Error" : "User Doesn't Exist"}
 
-@app.post("/users/update/{uname}")
-def user_update(uname : str, user : schemas.User, db : Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.userName == uname).first()
-    db.close()
-    return {"detail":"Doubts to be cleared! (Authentication)"}
+async def current_user(token : str = Depends(oauth2_scheme), db : Session = Depends(get_db)):
+    payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+    user = db.query(models.User).filter(models.User.userName == payload.get('userName')).first()
+    return user
+
+@app.post("/users/update/")
+def user_update(user = Depends(current_user)):
+    return user
 
 def to_base_62():
     deci = random.randint(1,10)
